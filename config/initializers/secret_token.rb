b72fef243a5a5df99bcee6ccd13a092b4e3e6901
Haldun Bayhantopcu <hb@haldun.me>
2011-10-11 22:22:18 +0300

    Initial crap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetingsapp::Application.config.secret_token = '241d8b76405f24e42b3d4ed0920c4f8779229ad79418832f3b07bf8543b04f59acf0359940bc95c10d1a54d7918f8556013a98c2779850cb3bd1b33595cbcac8'

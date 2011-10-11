ad796f09fb834c301f3c43e452d6f49489f3f300
Haldun Bayhantopcu <hb@haldun.me>
2011-10-12 01:35:31 +0300

    some redis crap

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ActiveRecord::Base
+  extend ActiveSupport::Memoizable
+
   belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
   has_many :memberships, :dependent => :destroy
   has_many :admins, :through => :memberships, :class_name => "User", :source => :user,
@@ -9,10 +11,37 @@ class Room < ActiveRecord::Base
   validates_presence_of :name
 
   before_create { generate_token(:token) }
+  after_save { Rails.cache.write("rooms/#{id}", self) }
+  after_destroy { Rails.cache.delete("rooms/#{id}") }
+
+  def self.fetch(id)
+    Rails.cache.fetch("rooms/#{id}") { find(id) }
+  end
 
   def generate_token(column)
     begin
       self[column] = SecureRandom.urlsafe_base64(32)
     end while Room.exists?(column => self[column])
   end
+
+  def join!(user)
+    REDIS.zadd(redis_key(:online_users), user.id, Time.now.utc)
+  end
+
+  def leave!(user)
+    REDIS.srem(redis_key(:online_users), user.id)
+  end
+
+  def online_users
+    @online_users ||= begin
+      user_ids = REDIS.smembers(redis_key(:online_users))
+      user_ids.present? ? User.where(:id => user_ids) : []
+    end
+  end
+
+  def redis_key(prefix)
+    "rooms/#{self.id}/#{prefix}"
+  end
+
+  memoize :redis_key
 end
